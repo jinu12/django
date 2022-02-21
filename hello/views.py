ce25ac7e4e221cdeccfd3a75b952a42865d7a7b2
jinu12 <kzkju@naver.com>
2022-02-21 13:57:26 +0900

    add html and upgrade hello.views commit

--- hello/views.py
+++ hello/views.py
@@ -9,3 +9,15 @@ class MyView(View):
     @request_mapping("/", method="get")
     def home(self, request):
         return render(request, 'home.html')
+
+    @request_mapping("/next", method="get")
+    def next(self, request):
+        return render(request, 'next.html')
+
+    @request_mapping("/next2", method="get")
+    def next2(self, request):
+        return render(request, 'next2.html')
+
+    @request_mapping("/next3", method="get")
+    def next3(self, request):
+        return render(request, 'next3.html')
