0444d5b3051ed5e210afb7497086d543264aeaa4
jinu12 <kzkju@naver.com>
2022-02-22 14:37:08 +0900

    bootstrap commit

--- hello/views.py
+++ hello/views.py
@@ -7,45 +7,5 @@ from django_request_mapping import request_mapping
 class MyView(View):
 
     @request_mapping("/", method="get")
-    def home(self, request):
-        return render(request, 'home.html')
-
-    @request_mapping("/next", method="get")
-    def next(self, request):
-        return render(request, 'next.html')
-
-    @request_mapping("/next2", method="get")
-    def next2(self, request):
-        return render(request, 'next2.html')
-
-    @request_mapping("/next3", method="get")
-    def next3(self, request):
-        return render(request, 'next3.html')
-
-    @request_mapping("/next4", method="get")
-    def next4(self, request):
-        return render(request, 'next4.html')
-
-    @request_mapping("/register", method="get")
-    def register(self, request):
-        return render(request, 'register.html')
-
-    @request_mapping('/css', method="get")
-    def css(self, request):
-        return render(request, 'css.html')
-
-    @request_mapping('/css2', method="get")
-    def css2(self, request):
-        return render(request, 'css2.html')
-
-    @request_mapping('/css3', method="get")
-    def css3(self, request):
-        return render(request, 'css3.html')
-
-    @request_mapping('/css4', method="get")
-    def css4(self, request):
-        return render(request, 'css4.html')
-
-    @request_mapping('/css5', method="get")
-    def css5(self, request):
-        return render(request, 'css5.html')
+    def index(self, request):
+        return render(request, 'index.html')
