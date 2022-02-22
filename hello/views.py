e0d8cf789f3f183d6bd1d4d4e62fb5006cc9e424
jinu12 <kzkju@naver.com>
2022-02-22 11:01:20 +0900

    add css1~5 and update views commit

--- hello/views.py
+++ hello/views.py
@@ -29,3 +29,23 @@ class MyView(View):
     @request_mapping("/register", method="get")
     def register(self, request):
         return render(request, 'register.html')
+
+    @request_mapping('/css', method="get")
+    def css(self, request):
+        return render(request, 'css.html')
+
+    @request_mapping('/css2', method="get")
+    def css2(self, request):
+        return render(request, 'css2.html')
+
+    @request_mapping('/css3', method="get")
+    def css3(self, request):
+        return render(request, 'css3.html')
+
+    @request_mapping('/css4', method="get")
+    def css4(self, request):
+        return render(request, 'css4.html')
+
+    @request_mapping('/css5', method="get")
+    def css5(self, request):
+        return render(request, 'css5.html')
